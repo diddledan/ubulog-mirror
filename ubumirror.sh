a820727f87c93703fc1c3bcdd4f4dffcebeb1b9e
Dani Llewellyn <diddledani@ubuntu.com>
2021-12-14 13:53:56 +0000

    Improve script restart recovery
    Signed-off-by: Dani Llewellyn <diddledani@ubuntu.com>

--- ubumirror.sh
+++ ubumirror.sh
@@ -31,16 +31,20 @@ do_sync() {
     while [ "$date" != "$end" ]; do
         wget --quiet -c -l inf -m -N -np -t inf \
             --random-wait --retry-connrefused \
-            -A "*.txt" -R "\#*.html" "http://irclogs.ubuntu.com/$date/"
+            -A "*.txt" -R "*.html" "http://irclogs.ubuntu.com/$date/"
 
-        for channel_file in $(ls "$DIR/mirror/irclogs.ubuntu.com/$date/"\#*.txt); do
+        for channel_file in "$DIR/mirror/irclogs.ubuntu.com/$date/#"*.txt; do
             channel="$(basename "$channel_file" .txt)"
 
             mkdir -p "$DIR/spool/$channel"
             sourcecodec="$(/usr/bin/uchardet "$channel_file" | awk '{print $1}')"
             tmp_file="$(mktemp)"
             final_file="$DIR/spool/$channel/$channel.$(date --date "$date" +'%Y-%m-%d')"
-            iconv -f "${sourcecodec}" -t UTF-8 "$channel_file" >"$tmp_file"
+            if [ "$sourcecodec" = "unknown" ]; then
+                cp "$channel_file" "$tmp_file"
+            else
+                iconv -f "${sourcecodec}" -t UTF-8 "$channel_file" >"$tmp_file"
+            fi
             touch "$final_file";
             # chmod 644 "$final_file"
             rsync --inplace "$tmp_file" "$final_file"
@@ -48,19 +52,24 @@ do_sync() {
         done
 
         echo "$date done."
+        echo "$date" > last_sync_date
 
         date="$(date --date "$date + 1 days" +'%Y/%m/%d')"
     done
 }
 
 # sync everything on startup
-do_sync "$DIR" "2004/07/05" "$(date "yesterday" +'%Y/%m/%d')"
+start_date="2004/07/05"
+if [ -f last_sync_date ]; then
+    start_date=$(cat last_sync_date)
+fi
+do_sync "$DIR" "$start_date" "$(date --date "yesterday 23:59" +'%Y/%m/%d')"
 
 # run a loop fetching today's logs (30-minute intervals)
 while (true); do
     # do yesterday, today, and "tomorrow" to naively cope with timezones because I don't know what timezone the bot runs under
-    do_sync "$DIR" "$(date --date "yesterday" +'%Y/%m/%d')" "$(date --date "yesterday" +'%Y/%m/%d')"
-    # wait for 86400 seconds (24 hours) before running again
+    do_sync "$DIR" "$(date --date "yesterday 00:00" +'%Y/%m/%d')" "$(date --date "yesterday 23:59" +'%Y/%m/%d')"
+    # wait for 24 hours before running again
     sleep 86400
 done
 
